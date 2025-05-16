c8025507f6d4226423321cf02c2ef7fddb1766e5
Maksym Panasovskyi <ie.panasovskyi@gmail.com>
2025-05-16 20:00:40 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('.field-text')];
+const labelTextMap = {
+  firstName: 'First Name',
+  lastName: 'Last Name',
+  email: 'Email',
+  password: 'Password',
+};
+
+inputs.forEach((input) => {
+  const label = document.createElement('label');
+
+  label.htmlFor = input.id;
+  label.className = 'field-label';
+
+  input.insertAdjacentElement('beforebegin', label);
+
+  if (input.name in labelTextMap) {
+    label.textContent = labelTextMap[input.name];
+  }
+  input.placeholder = label.textContent = labelTextMap[input.name];
+});
